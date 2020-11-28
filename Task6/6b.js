37a9bcff35f4a27ae12b7053762c53e1e2bd8edd
burakhanbakir <burakhanbakir@gmail.com>
2020-11-28 12:30:46 +0100

    Task6

---
+++ Task6/6b.js
@@ -0,0 +1,14 @@
+function show1() {
+    document.getElementById("div1").style.visibility = "visible";
+}
+setTimeout("show1()", 3000); 
+
+function show2() {
+    document.getElementById("div2").style.visibility = "visible";
+}
+setTimeout("show2()", 5000); 
+
+function show3(){
+  document.getElementById("div3").style.visibility = "visible";
+}
+setTimeout("show3()",10000);
