94a92198fe5a0e28109986075863cc520542d8df
burakhanbakir <burakhanbakir@gmail.com>
2020-12-12 18:08:13 +0100

    Task8

---
+++ Task8/Members.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+const members = [{
+        id: 1,
+        name: 'Burakhan Bakir',
+        email: 'burakhanbakir@gmail.com',
+        country: 'Turkey',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Paulina Kulesza',
+        email: 'paulina@gmail.com',
+        country: 'Poland',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Manuel Neuer',
+        email: 'man@gmail.com',
+        country: 'Germany',
+        status: 'active'
+    },
+];
+
+module.exports = members;
